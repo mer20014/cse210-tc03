b25c30b4901d8b3b9c5cfe8b4815a4dc87bcad27
Marco Varela <85329426+MarcoAntonioVarela@users.noreply.github.com>
2021-10-02 22:06:13 -0600

    Update main.py

--- tc03part2/main.py
+++ tc03part2/main.py
@@ -5,9 +5,13 @@ print('Welcome to our colaborative project')
 def call_jessen():
   print('Hello from Brendan')
   
+def call_marco():
+    print('Hello from Marco')
+
 def main():
   call_jessen()
   call_merrill()
+  call_marco()
 
 if __name__ == __main__:  
   main()
