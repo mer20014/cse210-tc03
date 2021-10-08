af415bde9ff3562388a24321cbf422511c7dd66e
youngm <dizzytmy@byui.edu>
2021-10-08 11:20:07 -0600

    fixed main file and added imports.

--- tc03part2/main.py
+++ tc03part2/main.py
@@ -1,18 +1,23 @@
-from merrill import *
-from varela import *
+from merrill import call_merrill
+from varela import call_marco
+from young import call_matt
+from jessen import call_jessen
 
 print('Welcome to our colaborative project')
-
-def call_jessen():
-  print('Hello from Brendan')
+call_merrill()
+call_marco()
+call_matt()
+call_jessen()
+#def call_jessen():
+  #print('Hello from Brendan')
 
 #Updated function (10/06/2021)
 
-def call_marco():
-    print('Hello from Marco')
+#def call_marco():
+    #print('Hello from Marco')
 
-def call_matt():
-    print('Hello from Matthew')
+#def call_matt():
+    #print('Hello from Matthew')
     
 def main():
   call_jessen()
@@ -20,5 +25,5 @@ def main():
   call_marco()
   call_matt()
 
-if __name__ == __main__:  
+if __name__ == main:  
   main()
